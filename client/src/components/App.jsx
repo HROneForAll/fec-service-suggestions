9113301ca2c946410b40945344c2d1a9440977f0
Michael Gagujas <mikey@Michaels-MacBook-Pro.local>
2018-12-20 19:55:57 -0800

    makes axios request and renders Houses data

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
-import $ from 'jquery';
+import axios from 'axios';
+import HousesList from './HousesList.jsx';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props)
-	}
-	render() {
-		return(
-			<div>
-			  testing
-			</div>
-			)
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      houses: [],
+      moreRevealed: false
+    };
+    this.toggleMoreHomes = this.toggleMoreHomes.bind(this);
+  }
+  componentDidMount() {
+    this.getHomes(); 
+  }
+  getHomes() {
+    axios.get('/suggestions')
+      .then((data) => {
+        this.setState({houses: data.data});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  toggleMoreHomes() {
+    this.setState({moreRevealed: !this.state.moreRevealed});
+  }
+  render() {
+    return (
+      <div>
+        <h1>Other highly rated homes</h1>
+        <HousesList toggle={this.toggleMoreHomes} state={this.state}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/HousesList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import House from './House.jsx';
+
+class HousesList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  toggle() {
+    this.props.toggle();
+  }
+  render() {
+    return (
+      <div>
+        <div className="HousesList">
+          {this.props.state.houses.slice(0, 4).map((house, index) => 
+            <House house={house} key={index}/>
+          )}
+        </div>
+        {this.props.state.moreRevealed ? 
+          <div>
+            <div className="HousesList">{this.props.state.houses.slice(4, 8).map((house, index) => 
+              <House house={house} key={index}/>)}
+            </div>
+            <div className="HousesList">{this.props.state.houses.slice(8, 12).map((house, index) => 
+              <House house={house} key={index}/>)}
+            </div>
+          </div>
+          : 
+          <a href="#" id="displayMore" className="verified" onClick={this.toggle.bind(this)}>Show more homes</a>
+        }
+      </div>
+    );
+  }
+}
+
+export default HousesList;
