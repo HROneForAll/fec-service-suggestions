0c50c0ba88577ab91b69ed2a6b17a2b911f5db95
Michael Gagujas <mikey@Michaels-MacBook-Pro.local>
2018-12-31 15:34:02 -0800

    updated for jest and styled-components

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,24 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
+  mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?/,
+        exclude: /node_modules/,
         include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },        
       }
     ]
   }
