8570ff678c040ca2523f4ef3c2ce171615e7d505
Michael Gagujas <mikey@Michaels-MacBook-Pro.local>
2018-12-31 15:41:22 -0800

    code cleanup/removed promise.resolve

--- server/server.js
+++ server/server.js
@@ -6,26 +6,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
 
-app.get('/suggestions', function(req, res) {
-  Promise.resolve(db.find())
+app.get('/homes/:id/suggestions', function(req, res) {
+	let id = req.params.id
+  db.findHome(id)
     .then((results) => {
       res.status(200).send(results);
     }).catch((error) => {
       console.log(error);
     });
 });
-
-// app.get('/suggestions/:id', function(req, res) {
-// 	let id = req.params.id
-//   Promise.resolve(db.find({id: id}))
-//     .then((results) => {
-//     	console.log('server', results)
-//       res.status(200).send(results);
-//     }).catch((error) => {
-//       console.log(error);
-//     });
-// });
-
 
 const port = 3050;
 app.listen(port, () => {
