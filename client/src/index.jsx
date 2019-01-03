ed950225b117f815565aaa7f0b5e9658947e6d8b
Michael Gagujas <mikey@Michaels-MacBook-Pro.local>
2019-01-02 19:26:18 -0800

    capitalized S in suggestions

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Suggestions from './components/Suggestions.jsx';
 
-ReactDOM.render(<Suggestions />, document.getElementById('suggestions'));
+ReactDOM.render(<Suggestions />, document.getElementById('Suggestions'));
