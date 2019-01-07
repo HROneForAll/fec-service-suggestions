6be20b4ea5d2fe43fdbd2ea97d933799c17ad2c9
Michael Gagujas <mikey@Michaels-MacBook-Pro-2.local>
2019-01-07 10:36:48 -0800

    code cleanup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Suggestions from './components/Suggestions.jsx';
 
-
 ReactDOM.render(<Suggestions />, document.getElementById('Suggestions'));
