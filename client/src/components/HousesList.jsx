b7d4cabc24369b11c4e58ecdcf6a14d48e145c4f
Michael Gagujas <mikey@Michaels-MacBook-Pro-2.local>
2019-01-07 10:32:53 -0800

    made corrections for House props

--- client/src/components/HousesList.jsx
+++ client/src/components/HousesList.jsx
@@ -1,59 +1,47 @@
 import React from 'react';
 import House from './House.jsx';
-import { HousingList, Header, ShowMoreHomes } from './Styled_Components/styling.jsx';
+import { HousesListCss, HousingList, ShowMoreHomes } from './Styled_Components/styling.jsx';
 
-
-class HousesList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  toggleMoreHomes() {
-    this.props.toggleHomes();
-  }
-  render() {
-    return (
-      <div>
-        <HousingList>
-          {this.props.state.houses[0].suggestions.slice(0, 4).map((house, index) => 
-            <House 
-              house={house} 
-              key={index}
-              state={this.props.state}
-              toggleCreateList={this.props.toggleCreateList}
-              addFavoriteList={this.props.addFavoriteList}
-              favoriteHome = {this.props.favoriteHome}
-            />
-          )}
+const HousesList = (props) => (
+  <HousesListCss>
+    <HousingList>
+      {props.state.houses[0].suggestions.slice(0, 4).map((house, index) => 
+        <House 
+          house={house} 
+          key={index}
+          state={props.state}
+          addFavoriteList={props.addFavoriteList}
+          addToFavorited = {props.addToFavorited}
+          removeFromFavorited = {props.removeFromFavorited}
+        />
+      )}
+    </HousingList>
+    {props.state.moreRevealed 
+      ? <div>
+        <HousingList>{props.state.houses[0].suggestions.slice(4, 8).map((house, index) => 
+          <House 
+            house={house} 
+            key={index}
+            state={props.state}
+            addFavoriteList={props.addFavoriteList}
+            addToFavorited = {props.addToFavorited}
+            removeFromFavorited = {props.removeFromFavorited}
+          />)}
         </HousingList>
-        {this.props.state.moreRevealed 
-          ? <div>
-            <HousingList>{this.props.state.houses[0].suggestions.slice(4, 8).map((house, index) => 
-              <House 
-                house={house} 
-                key={index}
-                state={this.props.state}
-                toggleModal={this.props.toggleModal}
-                toggleCreateList={this.props.toggleCreateList}
-                addFavoriteList={this.props.addFavoriteList}
-              />)}
-            </HousingList>
-            <HousingList>{this.props.state.houses[0].suggestions.slice(8, 12).map((house, index) => 
-              <House 
-                house={house} 
-                key={index}
-                state={this.props.state}
-                toggleModal={this.props.toggleModal}
-                toggleCreateList={this.props.toggleCreateList}
-                addFavoriteList={this.props.addFavoriteList}
-              />)}
-            </HousingList>
-          </div>
-          : 
-          <ShowMoreHomes onClick={this.toggleMoreHomes.bind(this)}>Show more homes</ShowMoreHomes>
-        }
+        <HousingList>{props.state.houses[0].suggestions.slice(8, 12).map((house, index) => 
+          <House 
+            house={house} 
+            key={index}
+            state={props.state}
+            addFavoriteList={props.addFavoriteList}
+            addToFavorited = {props.addToFavorited}
+            removeFromFavorited = {props.removeFromFavorited}
+          />)}
+        </HousingList>
       </div>
-    );
-  }
-}
-
+      : <ShowMoreHomes onClick={props.toggleHomes}>Show more homes</ShowMoreHomes>
+    }
+  </HousesListCss>
+);
+  
 export default HousesList;
